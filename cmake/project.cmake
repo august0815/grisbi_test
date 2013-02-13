d01dd6d81071fe35ea7720d7ea69928eda9e67ce
august0815 <mario.marcec@googlmail.com>
2013-02-13 17:50:23 +0100

    Valama version 0.1

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -1,10 +1,27 @@
-set(project_name grisbi_test)
-set(grisbi_test_VERSION 0.0.1)
+# Diese Datei wurde autogeneriert von Valama 0.1.2. Bitte nicht ändern.
+set(project_name "grisbi_test")
+set(grisbi_test_VERSION "0.0.1")
 set(required_pkgs
-Sxml-0.1
-gee-1.0
-gio-2.0
-glib-2.0
-gobject-2.0
-gtk+-3.0
+"Sxml-0.1"
+"gee-1.0"
+"gio-2.0"
+"glib-2.0"
+"gobject-2.0"
+"gtk+-3.0"
+)
+set(srcfiles
+"src/Bank.vala"
+"src/Kategorie.vala"
+"src/Konto.vala"
+"src/MasterState.vala"
+"src/Party.vala"
+"src/Planer.vala"
+"src/Subkategorie.vala"
+"src/Transaktion.vala"
+"src/load_file.vala"
+"src/main.vala"
+"src/test.vala"
+)
+set(vapifiles
+"vapi/config.vapi"
 )
